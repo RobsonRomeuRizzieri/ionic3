c6b05b8a963aa5ca7fa3803f6baf1ba79d6d4c33
Robson Romeu Rizzieri <robsonromeu@gmail.com>
2018-09-22 16:33:40 -0300

    Adicionada pagina de detalhes e configurações de menu.

--- src/providers/moovie/moovie.ts
+++ src/providers/moovie/moovie.ts
@@ -13,7 +13,7 @@ import 'rxjs/add/operator/map';
 export class MoovieProvider {
 
   private baseApiPath = "https://api.themoviedb.org/3";
-  private api_key = "069cc1717a75d2af925e91349d8f6a97";
+  private api_key = "Criar sua Api Key";
 
   constructor(public http: HttpClient) {
     console.log('Hello MoovieProvider Provider');
