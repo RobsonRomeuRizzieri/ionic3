228f2e67d6ec47ce2891e295d9ca2f7bbbabb78e
Robson Romeu Rizzieri <robsonromeu@gmail.com>
2018-09-22 16:32:46 -0300

    Adicionado camera na pagina de perfil.

--- src/providers/moovie/moovie.ts
+++ src/providers/moovie/moovie.ts
@@ -13,7 +13,7 @@ import 'rxjs/add/operator/map';
 export class MoovieProvider {
 
   private baseApiPath = "https://api.themoviedb.org/3";
-  private api_key = "criar sua api key para conectar";
+  private api_key = "069cc1717a75d2af925e91349d8f6a97";
 
   constructor(public http: HttpClient) {
     console.log('Hello MoovieProvider Provider');
